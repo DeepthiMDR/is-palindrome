5b9a78e24dd0dbbb67ec52c36bbf3e96ef09fe05
DeepthiMDR <50893888+DeepthiMDR@users.noreply.github.com>
2019-07-21 08:40:38 +0530

    update the logic.

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,5 +1,29 @@
 public class Palindrome {
     public boolean check(String sentence) {
-        return false;
-    }
+        int length = sentence.length()-1;
+        String pattern = "[a-zA-Z0-9]";
+
+        int i = 0;
+        while(i < (sentence.length())/2)
+        {
+            String ch = sentence.charAt(i) + "";
+            String ch2 = sentence.charAt(length)+ "";
+
+            if(ch.matches(pattern) && ch2.matches(pattern)){
+                if(!ch2.equalsIgnoreCase(ch))
+                    return false;
+                i++;
+                length--;
+            }
+            else if (!ch.matches(pattern))
+                i++;
+
+            else if (!ch2.matches(pattern))
+                length--;
+
+        }
+        return true;
+    
+
+   }
 }
